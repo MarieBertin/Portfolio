d5f3cfb1c6cc6ffcf81dbfc9eefc053fc0ad6d91
BOUYADU44 <marie.bertin0905@gmail.com>
2025-04-25 21:10:41 +0200

    modif browserrout in hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import Education from "./pages/Education";
 import Other from "./pages/Other";
@@ -18,7 +20,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/education" element={<Education />} />
@@ -27,9 +29,10 @@ const App = () => (
           <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
+
 
 export default App;
